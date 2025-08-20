f7b2cad6926119dd4677d8b9ee509c6f2f50e5e3
Andy150104 <tranduyanh7766@gmail.com>
2025-08-20 08:12:46 +0700

    [Add] Add logic video, handle pause, resume

--- src/app/test/video/page.tsx
+++ src/app/test/video/page.tsx
@@ -1,4 +1,6 @@
-import VideoPlayer from "EmoEase/components/Video/VideoPlayer";
+"use client";
+import { Button } from "antd";
+import VideoPlayer, { PauseReason } from "EmoEase/components/Video/VideoPlayer";
 
 export default function VideoPage() {
   const hlsUrl =
@@ -10,6 +12,20 @@ export default function VideoPage() {
     "https://res.cloudinary.com/dbfokyruf/video/upload/sp_auto:subtitles_((code_vi-VN;file_Hng-Trm---Ngc-2-Hy--Em-Qun-Official-MV.vi-VN.vtt))/v1754995566/Hng-Trm---Ngc-2-Hy--Em-Qun-Official-MV.m3u8";
   const vttUrl =
     "https://res.cloudinary.com/dbfokyruf/raw/upload/v1754995569/Hng-Trm---Ngc-2-Hy--Em-Qun-Official-MV.vi-VN.vtt";
+  const handlePause = (info: {
+    currentTime: number;
+    duration: number;
+    reason: PauseReason;
+  }) => {
+    console.log("Paused:", info);
+    // TODO: gọi API lưu progress, analytics, v.v...
+  };
+  
+
+const makeOverlayClose = (id: string, close: () => void) => () => {
+  console.log(`overlay ${id} close clicked`);
+  close();
+};
 
   return (
     <main className="p-5">
@@ -18,6 +34,114 @@ export default function VideoPage() {
         src={hlsUrl}
         // poster="https://media.istockphoto.com/id/2150399781/vi/anh/m%E1%BB%99t-c%C3%A1nh-%C4%91%E1%BB%93ng-n%C3%B4ng-nghi%E1%BB%87p-r%E1%BB%99ng-l%E1%BB%9Bn-v%E1%BB%9Bi-nh%E1%BB%AFng-c%C3%A2y-ng%C3%B4-tr%E1%BB%93ng-ng%C3%B4-%E1%BB%9F-quy-m%C3%B4-c%C3%B4ng-nghi%E1%BB%87p-c%E1%BA%A3nh-quan.jpg?s=2048x2048&w=is&k=20&c=IX8bx3uLJS71IP7WHB3mvlj7pKJrx5T-YG6_pCXNrko="
         urlVtt={vttUrl}
+        onPause={handlePause}
+        onResume={(info) =>
+          console.log(`Resumed after ${info.pausedForMs}ms`, info)
+        }
+        timedOverlays={[
+          {
+            id: "quiz-150",
+            start: 3, // 2:30
+            pauseOnShow: true, // tự pause khi xuất hiện
+            backdrop: true, // mặc định true, có thể bỏ
+            className: "absolute inset-0 !z-50 h-full w-full bg-black/50",
+            content: ({ close }) => (
+              <div className="h-full w-full bg-white p-6 overflow-auto">
+                {/* Nếu muốn nội dung ở giữa, dùng flex */}
+                <div className="mx-auto flex h-full max-w-2xl flex-col justify-center">
+                  <h3 className="mb-3 text-lg font-semibold">Quiz ôn tập 1</h3>
+                  <p className="mb-3 font-medium">
+                    Trong React, Hook nào để thêm state vào functional
+                    component?
+                  </p>
+
+                  <ul className="space-y-2">
+                    <li>
+                      <label className="flex items-center gap-2">
+                        <input type="radio" name="q1" /> useContext
+                      </label>
+                    </li>
+                    <li>
+                      <label className="flex items-center gap-2">
+                        <input type="radio" name="q1" /> useEffect
+                      </label>
+                    </li>
+                    <li>
+                      <label className="flex items-center gap-2">
+                        <input type="radio" name="q1" /> useReducer
+                      </label>
+                    </li>
+                    <li>
+                      <label className="flex items-center gap-2">
+                        <input type="radio" name="q1" /> <b>useState</b>
+                      </label>
+                    </li>
+                  </ul>
+
+                  <div className="mt-6 self-end">
+                    <Button
+                      onClick={makeOverlayClose("quiz-003", close)}
+                      className="rounded-lg bg-indigo-600 px-4 py-2 text-white pointer-events-auto"
+                    >
+                      Tiếp tục xem
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            ),
+          },
+          {
+            id: "quiz-151",
+            start: 6, // 2:30
+            pauseOnShow: true, // tự pause khi xuất hiện
+            // backdrop: true,    // mặc định true, có thể bỏ
+            className: "absolute inset-0 z-50 h-full w-full bg-black/50",
+            content: ({ close }) => (
+              <div className="h-full w-full bg-white p-6 overflow-auto">
+                {/* Nếu muốn nội dung ở giữa, dùng flex */}
+                <div className="mx-auto flex h-full max-w-2xl flex-col justify-center">
+                  <h3 className="mb-3 text-lg font-semibold">Quiz ôn tập 2</h3>
+                  <p className="mb-3 font-medium">
+                    Trong React, Hook nào để thêm state vào functional
+                    component?
+                  </p>
+
+                  <ul className="space-y-2">
+                    <li>
+                      <label className="flex items-center gap-2">
+                        <input type="radio" name="q1" /> useContext
+                      </label>
+                    </li>
+                    <li>
+                      <label className="flex items-center gap-2">
+                        <input type="radio" name="q1" /> useEffect
+                      </label>
+                    </li>
+                    <li>
+                      <label className="flex items-center gap-2">
+                        <input type="radio" name="q1" /> useReducer
+                      </label>
+                    </li>
+                    <li>
+                      <label className="flex items-center gap-2">
+                        <input type="radio" name="q1" /> <b>useState</b>
+                      </label>
+                    </li>
+                  </ul>
+
+                  <div className="mt-6 self-end">
+                    <Button
+                      onClick={close}
+                      className="rounded-lg bg-indigo-600 px-4 py-2 text-white"
+                    >
+                      Tiếp tục xem
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            ),
+          },
+        ]}
       />
     </main>
   );
