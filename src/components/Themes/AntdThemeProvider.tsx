01e3ee752887a9541d930ebc01ae39bb88057936
Andy150104 <tranduyanh7766@gmail.com>
2025-08-28 20:25:35 +0700

    [Fix] Fix theme mode

--- src/components/Themes/AntdThemeProvider.tsx
+++ src/components/Themes/AntdThemeProvider.tsx
@@ -1,30 +1,28 @@
 // src/AntdThemeProvider.tsx
 "use client";
-import React from "react";
+import React, { useMemo } from "react";
 import { ConfigProvider, theme as antdTheme } from "antd";
 import { useTheme } from "EmoEase/Provider/ThemeProvider";
 
 export function AntdThemeProvider({ children }: { children: React.ReactNode }) {
   const { isDarkMode } = useTheme();
 
-  return (
-    <ConfigProvider
-      key={isDarkMode ? "dark" : "light"}
-      theme={{
-        cssVar: true,
-        algorithm: isDarkMode
-          ? antdTheme.darkAlgorithm
-          : antdTheme.defaultAlgorithm,
-        token: isDarkMode
-          ? {
-              colorBgLayout: "#020712",
-              colorBgContainer: "#050D18",
-              colorText: "#ffffff",
-            }
-          : {},
-      }}
-    >
-      {children}
-    </ConfigProvider>
+  const theme = useMemo(
+    () => ({
+      cssVar: true,
+      algorithm: isDarkMode
+        ? antdTheme.darkAlgorithm
+        : antdTheme.defaultAlgorithm,
+      token: isDarkMode
+        ? {
+            colorBgLayout: "#020712",
+            colorBgContainer: "#050D18",
+            colorText: "#ffffff",
+          }
+        : {},
+    }),
+    [isDarkMode],
   );
+
+  return <ConfigProvider theme={theme}>{children}</ConfigProvider>;
 }
